6fe93c81f5fecac822e130d9b8cc494ec28861eb
Droid664 <iskandarov.murat@gmail.com>
2024-04-30 15:15:01 +0500

    Добавил компонент Button

---
+++ app/src/shared/ui/Button/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <button
+    @click="onClick"
+    :class="[modClass ? `button--${modClass}` : '']"
+    class="button"
+    :type="type"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts" setup>
+interface IProps {
+  modClass?: 'danger'
+  type?: 'button' | 'submit' | 'reset'
+  disabled?: boolean
+  onClick?: (e: MouseEvent) => void
+}
+
+const { modClass, type, onClick } = withDefaults(defineProps<IProps>(), {
+  type: 'button',
+})
+</script>
+
+<style lang="scss" scoped>
+.button {
+  display: inline-flex;
+  align-items: center;
+  gap: 6px;
+  padding-inline: 12px;
+  height: 28px;
+  border-radius: 4px;
+  border: 2px solid var(--white);
+  background: var(--black);
+  color: var(--white);
+  cursor: pointer;
+}
+</style>
